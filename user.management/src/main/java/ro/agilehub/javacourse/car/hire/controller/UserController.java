5c87e64727c4ad92ac911ab3aab4fea87c49a4f5
DragomirAlin <dragomirdanielalin@Yahoo.com>
2020-11-12 15:16:59 +0200

    changed in user controller

--- user.management/src/main/java/ro/agilehub/javacourse/car/hire/controller/UserController.java
+++ user.management/src/main/java/ro/agilehub/javacourse/car/hire/controller/UserController.java
@@ -1,14 +1,13 @@
 package ro.agilehub.javacourse.car.hire.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import ro.agilehub.javacourse.car.hire.api.model.UserDTO;
 import ro.agilehub.javacourse.car.hire.api.specification.UserApi;
+import ro.agilehub.javacourse.car.hire.exception.NotFoundException;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,10 +26,7 @@ public class UserController implements UserApi {
     @Override
     public ResponseEntity<UserDTO> getUser(Integer id) {
         Optional<UserDTO> user = usersList.stream().filter(userDTO -> userDTO.getId().equals(id)).findFirst();
-//        return ResponseEntity.ok(user.orElseThrow();
-
-        return (ResponseEntity<UserDTO>) ResponseEntity.notFound();
-
+        return ResponseEntity.ok(user.orElseThrow(NotFoundException::new));
     }
 
     @Override
@@ -41,6 +37,6 @@ public class UserController implements UserApi {
     @Override
     public ResponseEntity<Void> removeUser(Integer id) {
         usersList.remove(id - 1);
-        return (ResponseEntity<Void>) ResponseEntity.ok();
+        return ResponseEntity.ok().build();
     }
 }
---
+++ user.management/src/main/java/ro/agilehub/javacourse/car/hire/exception/NotFoundException.java
@@ -0,0 +1,4 @@
+package ro.agilehub.javacourse.car.hire.exception;
+
+public class NotFoundException extends RuntimeException{
+}

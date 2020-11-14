afd0e8cc46460a072dce20c30bfa5808abf01d6a
DragomirAlin <dragomirdanielalin@Yahoo.com>
2020-11-14 12:28:16 +0200

    added fleet api

--- user.management/src/main/java/ro/agilehub/javacourse/car/hire/controller/UserController.java
+++ user.management/src/main/java/ro/agilehub/javacourse/car/hire/controller/UserController.java
@@ -1,15 +1,19 @@
 package ro.agilehub.javacourse.car.hire.controller;
 
-import io.swagger.models.auth.In;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.fge.jsonpatch.JsonPatch;
+import com.github.fge.jsonpatch.JsonPatchException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-import ro.agilehub.javacourse.car.hire.api.model.InlineResponse201;
 import ro.agilehub.javacourse.car.hire.api.model.UserDTO;
 import ro.agilehub.javacourse.car.hire.api.specification.UserApi;
 import ro.agilehub.javacourse.car.hire.exception.NotFoundException;
 
 import javax.validation.Valid;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -17,7 +21,9 @@ import java.util.Optional;
 @RestController
 public class UserController implements UserApi {
 
+    ObjectMapper objectMapper = new ObjectMapper();
     ArrayList<UserDTO> usersList = new ArrayList<>();
+
 
     @Override
     public ResponseEntity<Integer> addUser(@Valid UserDTO userDTO) {
@@ -29,7 +35,7 @@ public class UserController implements UserApi {
 
     @Override
     public ResponseEntity<UserDTO> getUser(Integer id) {
-        Optional<UserDTO> user = usersList.stream().filter(userDTO -> userDTO.getId().equals(id)).findFirst();
+        Optional<UserDTO> user = getUserById(id);
         return ResponseEntity.ok(user.orElseThrow(NotFoundException::new));
     }
 
@@ -38,9 +44,35 @@ public class UserController implements UserApi {
         return ResponseEntity.ok(usersList);
     }
 
+//    @Override
+//    public ResponseEntity<UserDTO> updateUser(Integer id, ro.agilehub.javacourse.car.hire.api.model.@Valid JsonPatch jsonPatch) {
+//        try {
+//            UserDTO user = getUserById(id).orElseThrow(NotFoundException::new);
+//            JsonPatch jsonp = JsonPatch.fromJson(jsonPatch);
+//            UserDTO userPatched = applyPatchToUser(jsonPatch, user);
+//            //update method
+//            return ResponseEntity.ok().build();
+//        } catch (JsonPatchException | JsonProcessingException e) {
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+//        } catch (NotFoundException | IOException e) {
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+//        }
+//    }
+
     @Override
     public ResponseEntity<Void> removeUser(Integer id) {
         usersList.remove(id - 1);
         return ResponseEntity.ok().build();
     }
+
+    private Optional<UserDTO> getUserById(Integer id) {
+        Optional<UserDTO> userDTO = usersList.stream().filter(user -> user.getId().equals(id)).findFirst();
+        return userDTO;
+    }
+
+    private UserDTO applyPatchToUser(JsonPatch patch, UserDTO targerUser) throws JsonPatchException, JsonProcessingException {
+        JsonNode patched = patch.apply(objectMapper.convertValue(targerUser, JsonNode.class));
+        return objectMapper.treeToValue(patched, UserDTO.class);
+    }
+
 }
